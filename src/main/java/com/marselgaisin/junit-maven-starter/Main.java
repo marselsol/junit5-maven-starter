f7c3ebdad4e44f2be2111d821cb779850c2e8056
marselsol <marselgaisin@gmail.com>
2023-01-28 15:18:52 +0300

    Initial commit

---
+++ src/main/java/com/marselgaisin/junit-maven-starter/Main.java
@@ -0,0 +1,5 @@
+package com.marselgaisin.junit-maven-starter;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/marselgaisin/junit/service/UserService.java
@@ -0,0 +1,2 @@
+package com.marselgaisin.junit.service;public class UserService {
+}

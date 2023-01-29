8d5b8991d7580b051a5c7918184f590b3d59445e
marselsol <marselgaisin@gmail.com>
2023-01-29 15:28:53 +0300

    6 lesson

--- src/test/java/com/marselgaisin/junitTest/service/UserServiceTest.java
+++ src/test/java/com/marselgaisin/junitTest/service/UserServiceTest.java
@@ -4,17 +4,22 @@ import com.marselgaisin.junit.dto.User;
 import com.marselgaisin.junit.service.UserService;
 import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
 
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+    private UserService userService;
     @BeforeAll
     void init() {
         System.out.println("Before all: " + this);
     }
-    private UserService userService;
+
     @BeforeEach
     void prepare() {
         System.out.println("Before each " + this);
@@ -28,10 +33,38 @@ public class UserServiceTest {
     }
 
     @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void logicFailPasswordIdNotCorrect() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void logicFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
         var users = userService.getAll();
         assertEquals(2, users.size());
     }
